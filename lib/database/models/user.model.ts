7d91ea4b87616a20961af54395dca461f5104f50
ChihTeng Ma <chihtengma416@gmail.com>
2023-12-24 02:03:14 -0500

    modified user model

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,14 +1,14 @@
-import { Document, Schema, model, models } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
-export interface ICategory extends Document {
-   _id: string;
-   name: string;
-}
-
-const CategorySchema = new Schema({
-   name: { type: String, required: true, unique: true }
+const UserSchema = new Schema({
+   clerkId: { type: String, required: true, unique: true },
+   email: { type: String, required: true, unique: true },
+   username: { type: String, required: true, unique: true },
+   firstName: { type: String, required: true },
+   lastName: { type: String, required: true },
+   photo: { type: String, required: true }
 });
 
-const Category = models.Category || model("Category", CategorySchema);
+const User = models.User || model("User", UserSchema);
 
-export default Category;
+export default User;
