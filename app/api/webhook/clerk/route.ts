7d3d0dd973e103a1c86d03a25a7b9adf61cf0257
ChihTeng Ma <chihtengma416@gmail.com>
2023-12-25 23:18:20 -0500

    adjust clerked authentication to allow username

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -59,7 +59,7 @@ export async function POST(req: Request) {
       const user = {
          clerkId: id,
          email: email_addresses[0].email_address,
-         username: email_addresses[0].email_address,
+         username: username!,
          firstName: first_name,
          lastName: last_name,
          photo: image_url
