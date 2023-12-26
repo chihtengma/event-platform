1e61b8cb1f7b7a9e8736a1459f0a1569b10f70a6
ChihTeng Ma <chihtengma416@gmail.com>
2023-12-25 22:19:51 -0500

    added '/' to middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-   publicRoutes: ["/", "events/:id", "api/webhook/clerk", "api/webhook/stripe", "api/uploadthing"],
-   ignoredRoutes: ["api/webhook/clerk", "api/webhook/stripe", "api/uploadthing"]
+   publicRoutes: ["/", "/events/:id", "/api/webhook/clerk", "/api/webhook/stripe", "/api/uploadthing"],
+   ignoredRoutes: ["/api/webhook/clerk", "/api/webhook/stripe", "/api/uploadthing"]
 });
 
 export const config = {
