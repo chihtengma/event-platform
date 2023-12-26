6442b4cb7cf294cdf322604346613c4dc7bc3511
ChihTeng Ma <chihtengma416@gmail.com>
2023-12-25 22:50:16 -0500

    replace username with email address

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -59,7 +59,7 @@ export async function POST(req: Request) {
       const user = {
          clerkId: id,
          email: email_addresses[0].email_address,
-         username: username!,
+         username: email_addresses[0].email_address,
          firstName: first_name,
          lastName: last_name,
          photo: image_url
