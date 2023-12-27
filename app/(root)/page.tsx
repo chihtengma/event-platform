f900e5dc1d0498f1eb012b9411f98af20d019e9c
ChihTeng Ma <chihtengma416@gmail.com>
2023-12-27 14:32:14 -0500

    project finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,21 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+   const page = Number(searchParams?.page) || 1;
+   const searchText = (searchParams?.query as string) || "";
+   const category = (searchParams?.category as string) || "";
+
    const events = await getAllEvents({
-      query: "",
-      category: "",
-      page: 1,
+      query: searchText,
+      category,
+      page,
       limit: 6
    });
 
@@ -41,7 +48,10 @@ export default async function Home() {
                Trust by <br /> Thousands of Events
             </h2>
 
-            <div className="flex w-full flex-col gap-5 md:flex-row">Search CategoryFilter</div>
+            <div className="flex w-full flex-col gap-5 md:flex-row">
+               <Search />
+               <CategoryFilter />
+            </div>
 
             <Collection
                data={events?.data}
@@ -49,8 +59,8 @@ export default async function Home() {
                emptyStateSubtext="Come back later"
                collectionType="All_Events"
                limit={6}
-               page={1}
-               totalPages={2}
+               page={page}
+               totalPages={events?.totalPages}
             />
          </section>
       </>
--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,15 +1,24 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/database/models/order.model";
+import { SearchParamProps } from "@/types";
 import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 
-const ProfilePage = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
    const { sessionClaims } = auth();
    const userId = sessionClaims?.userId as string;
 
-   const organizedEvents = await getEventsByUser({ userId, page: 1 });
+   const orderspage = Number(searchParams?.ordersPage) || 1;
+   const eventsPage = Number(searchParams?.eventsPage) || 1;
+
+   const orders = await getOrdersByUser({ userId, page: orderspage });
+
+   const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+   const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
 
    return (
       <>
@@ -23,18 +32,18 @@ const ProfilePage = async () => {
             </div>
          </section>
 
-         {/* <section className="wrapper mr-8">
+         <section className="wrapper mr-8">
             <Collection
-               data={events?.data}
+               data={orderedEvents}
                emptyTitle="No event tickets purchased yet"
                emptyStateSubtext="No worries - plenty of exicting events to explore!"
                collectionType="My_Tickets"
                limit={3}
-               page={1}
+               page={orderspage}
                urlParamName="ordersPage"
-               totalPages={2}
+               totalPages={orders?.totalPages}
             />
-         </section> */}
+         </section>
 
          {/* Events Organized */}
          <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center pt-5 md:py-10">
@@ -52,10 +61,10 @@ const ProfilePage = async () => {
                emptyTitle="No event have been created yet"
                emptyStateSubtext="Go create some now"
                collectionType="Events_Organized"
-               limit={6}
-               page={1}
+               limit={3}
+               page={eventsPage}
                urlParamName="eventsPage"
-               totalPages={2}
+               totalPages={organizedEvents?.totalPages}
             />
          </section>
       </>
