24707895030e3c35395a3c96f7cc3e3670dd0db7
ChihTeng Ma <chihtengma416@gmail.com>
2023-12-27 14:35:40 -0500

    fixed spelling

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -18,7 +18,7 @@ const OrdersPage = async ({ searchParams }: SearchParamProps) => {
          </section>
 
          <section className="wrapper mt-8">
-            <Search placeholder="Search buyer name..." />
+            <Search placeHolder="Search buyer name..." />
          </section>
 
          <section className="wrapper overflow-x-auto">
